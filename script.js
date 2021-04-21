a38e73bf469398b4421e5dc8e636a3dbbf31dc3c
Giulio_Pa <giuliop93@hotmail.it>
2021-04-21 15:50:23 +0200

    wip

--- script.js
+++ script.js
@@ -9,13 +9,18 @@ function getRandomNumbers (x){
     return randomNumbers;
 }
 
-var numeri = getRandomNumbers(5);
-console.log(numeri);
+var numbers = getRandomNumbers(5);
+console.log(numbers);
 // Da li parte un timer di 30 secondi.
-setTimeout(myFunction, 30000)
-
-     
+setTimeout(myFunction, 30000);
+function myFunction (){
+    alert("Ti restano 30 secondi per disinnescarmi")
+}
 //  }
 // Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
-
+var userChoice = prompt("Inserisci i 5 numeri o esplodo");
+var userChoice = [];
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+if (insertNumbers === correctNumbers){
+
+}
