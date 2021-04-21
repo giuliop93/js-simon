84ab5984083ef4bb3c605844794e2e615125880a
Giulio_Pa <giuliop93@hotmail.it>
2021-04-21 15:10:08 +0200

    help

---
+++ script.js
@@ -0,0 +1,19 @@
+// Un alert() espone 5 numeri generati casualmente.
+var fiveRandomNumbers = [];
+
+function getRandomNUmbers (five){
+    for(i = 0; i < 5; i++);
+    console.log(i)
+    return Math.random() * 10;
+}
+getRandomNUmbers(five)
+fiveRandomNumbers.push(getRandomNUmbers()) = [];
+
+// Da li parte un timer di 30 secondi.
+ setTimeout(myFunction, 30000);
+ function (myFunction){
+     
+ }
+// Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
