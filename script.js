9a363a9e0e718e122148e13d35ee2e1508f198df
Giulio_Pa <giuliop93@hotmail.it>
2021-04-22 16:23:54 +0200

    done

--- script.js
+++ script.js
@@ -15,23 +15,25 @@ alert("Memorizza i seguenti numeri " + numbers + " per provare a disinnescarmi."
 console.log(numbers);
 
 // Da li parte un timer di 30 secondi.
-setTimeout(myFunction, 30000);
+setTimeout(myFunction, 4000);
 function myFunction() {
-    alert("i 30 secondi sono scaduti");
+    alert("i 4 secondi sono scaduti");
     // Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
     //pushare i numeroInserito nell'array userChoice
     var userChoice = [];
+  
     var guessNumbers = [];
+
     for (i = 0; i < 5; i++) {
         var numeroInserito = parseInt(prompt("Inserisci un numero"));
         userChoice.push(numeroInserito);
     }
     for (i = 0; i < userChoice.length; i++) {
-        if (correctNumbers.includes(userChoice[i])) {
-            guessNumbers.push(userChoice[i])
+        if (numbers.includes(userChoice[i])) {
+            guessNumbers.push(userChoice[i]);
         }
     }
     // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
-        console.log("i numeri indovinati sono" + guessNumbers + "e sono in totale" + guessNumbers.length)
+        console.log("i numeri indovinati sono " + guessNumbers + " e sono in totale " + guessNumbers.length)
 }
 //  }
